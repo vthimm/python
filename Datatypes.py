a3bd0fe24ba4a39ed02f80eb1d9ecafbaf96b124
vthimm <vivekanandat08@gmail.com>
2020-04-21 01:19:27 +0530

    first commit

---
+++ Datatypes.py
@@ -0,0 +1,14 @@
+string1='vivek'
+string2='ananda'
+print("string1+string2=",string1+string2)
+print("string1*string2=",string1*2)
+print(string1[3:5])
+      
+print("srtingcount=",string1.find('vek'))
+print("stringreplaced=",string1.replace('ek','k'))
+print("stringsplit",string1.split(','))      
+print("strcount=",string1.count('v',1,4))
+print("strupper=",string1.upper())
+print("strmx=",max(string1))
+print("strmn=",min(string1))
+print(string1.isalpha())
